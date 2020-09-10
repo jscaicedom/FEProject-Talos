0a0fd61b4a1c422c25481ecdefec823b55401f50
Johana Sarai Caicedo <johanasaraicaicedo@Ricardos-MacBook-Pro.local>
2020-09-10 13:35:34 -0500

    corrections

--- index.js
+++ index.js
@@ -1,63 +1,13 @@
-var toggleButton = document.querySelector('.toggle-button');
-var mobileNav = document.querySelector('.header-mobile');
+let toggleButton = document.querySelector('.toggle-button');
+let mobileNav = document.querySelector('.header-mobile');
 
 function open_mobile_menu() {
-  var items = mobileNav.querySelector('.header-items');
+  let items = mobileNav.querySelector('.header-items');
   $(mobileNav).slideToggle(500, () => {
     mobileNav.classList.add('open-parent');
     items.classList.add('open');
   });
 }
-
-// Testimonial Slider Mobile
-
-var slideDT = document.querySelectorAll('.slide-desk-tab');
-var circlesDT = document.querySelector('.circles.desk-tab');
-var dotDT = circlesDT.querySelectorAll('.point');
-
-var slideMobile = document.querySelectorAll('.testimonial-box');
-var circlesMobile = document.querySelector('.circles.mobile');
-var dotM = circlesMobile.querySelectorAll('.point');
-
-var styles = ['mySlides', 'fade'];
-
-function changeSlides(max_width) {
-  let indexDT;
-  let indexM;
-  if (max_width.matches) {
-    for (indexDT = 0; indexDT < slideDT.length; indexDT++) {
-      styles.forEach((element) => {
-        slideDT[indexDT].classList.remove(element);
-      });
-      dotDT[indexDT].classList.remove('dot');
-    }
-
-    for (indexM = 0; indexM < slideMobile.length; indexM++) {
-      styles.forEach((element) => {
-        slideMobile[indexM].classList.add(element);
-      });
-      dotM[indexM].add('dot');
-    }
-  } else {
-    for (indexDT = 0; indexDT < slideDT.length; indexDT++) {
-      styles.forEach((element) => {
-        slideDT[indexDT].classList.add(element);
-      });
-      dotDT[indexDT].classList.add('dot');
-    }
-
-    for (indexM = 0; indexM < slideMobile.length; indexM++) {
-      styles.forEach((element) => {
-        slideMobile[indexM].classList.remove(element);
-      });
-      dotM[indexM].classList.remove('dot');
-    }
-  }
-}
-
-var max_width = window.matchMedia('(max-width: 576px)');
-changeSlides(max_width);
-max_width.addListener(changeSlides);
 
 // Testimonial Slider
 
@@ -70,9 +20,8 @@ function currentSlide(currentIndex) {
 }
 
 function showSlides(index) {
-  let i;
-  var slides = document.querySelectorAll('.mySlides');
-  var dots = document.querySelectorAll('.dot');
+  let slides = document.querySelectorAll('.mySlides');
+  let dots = document.querySelectorAll('.dot');
   if (index > slides.length) {
     slideIndex = 1;
   }
@@ -88,28 +37,15 @@ function showSlides(index) {
   });
   slides[slideIndex - 1].classList.remove('slide-ocult');
   slides[slideIndex - 1].classList.add('slide-visible');
-
-  var parent = slides[slideIndex - 1].parentElement;
-
-  parent.classList.remove('slide-ocult');
-  parent.classList.add('slide-visible');
-
-  var children = slides[slideIndex - 1].querySelectorAll('.testimonial-box');
-
-  children.forEach((element) => {
-    element.classList.remove('slide-ocult');
-    element.classList.add('slide-visible');
-  });
-  dots[slideIndex - 1].className += ' active';
 }
 
 //Drop down questions
 
-var selectedQuestions = document.querySelectorAll('.question');
+let selectedQuestions = document.querySelectorAll('.question');
 
 function openQuestion(question) {
   closeAll();
-  var text = question.querySelector('.question-text');
+  const text = question.querySelector('.question-text');
   if ($(text).is(':visible')) {
     $(text).slideUp();
   } else {
@@ -119,19 +55,18 @@ function openQuestion(question) {
 
 function closeAll() {
   selectedQuestions.forEach((element) => {
-    var text = element.querySelector('.question-text');
-    $(text).slideUp();
+    $(element.querySelector('.question-text')).slideUp();
   });
 }
 
 //header active sections
 
 // init controller
-var controller = new ScrollMagic.Controller({
+let controller = new ScrollMagic.Controller({
   globalSceneOptions: { duration: '100%' },
 });
 
-var sections = [
+let sections = [
   '#home',
   '#about',
   '#about-video',
@@ -141,7 +76,7 @@ var sections = [
   '#contact',
 ];
 
-var header_sections = [
+let headerSections = [
   '#home-button',
   '#about-button',
   '#about-video-button',
@@ -152,9 +87,8 @@ var header_sections = [
 ];
 
 // build scenes
-let i;
-for (i = 0; i < sections.length; i++) {
-  new ScrollMagic.Scene({ triggerElement: sections[i] })
-    .setClassToggle(header_sections[i], 'active-header') // add class toggle
+sections.forEach(function (element, index) {
+  new ScrollMagic.Scene({ triggerElement: element })
+    .setClassToggle(headerSections[index], 'active-header') // add class toggle
     .addTo(controller);
-}
+});
